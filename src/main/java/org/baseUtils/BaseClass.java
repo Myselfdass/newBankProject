c440f13fa6c9250831e00c5e3277003fec0a6e44
Myselfdass <110597760+Myselfdass@users.noreply.github.com>
2022-10-11 13:09:52 +0530

    Update BaseClass.java

--- src/main/java/org/baseUtils/BaseClass.java
+++ src/main/java/org/baseUtils/BaseClass.java
@@ -16,7 +16,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class BaseClass {
 
 	public WebDriver driver;
-	public JavascriptExecutor script;
+	public JavascriptExecutor script = (Java);
 	
 	
 	public void browserLaunch(String browser) {
